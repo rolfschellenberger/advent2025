a690a7bfc94eef06eff2d7f15aefd59d2acd4a36
Rolf <rolf.schellenberger@fastnedcharging.com>
2025-11-29 22:50:29 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2025
+https://adventofcode.com/2025
+
+## Convert assignments to markdown
+https://www.convertsimple.com/convert-html-to-markdown/
---
+++ src/main/kotlin/com/rolf/day01/Day01.kt
@@ -0,0 +1,15 @@
+package com.rolf.day01
+
+import com.rolf.Day
+
+fun main() {
+    Solve().run()
+}
+
+class Solve : Day() {
+    override fun solve1(lines: List<String>) {
+    }
+
+    override fun solve2(lines: List<String>) {
+    }
+}
