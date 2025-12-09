ba8589d950d3bd653afef7e524adbce9f2cea806
Rolf <rolf.schellenberger@fastnedcharging.com>
2025-12-09 06:54:29 +0100

    Day 9

---
+++ src/main/kotlin/com/rolf/day09/Day09.kt
@@ -0,0 +1,88 @@
+package com.rolf.day09
+
+import com.rolf.Day
+import com.rolf.util.Block
+import com.rolf.util.Point
+import kotlin.math.absoluteValue
+
+fun main() {
+    Solve().run()
+}
+
+class Solve : Day() {
+    override fun solve1(lines: List<String>) {
+        val points = parsePoints(lines)
+        var largest = 0L
+        for (p1 in points) {
+            for (p2 in points) {
+                val width = (p1.x - p2.x).absoluteValue + 1L
+                val height = (p1.y - p2.y).absoluteValue + 1L
+                val surface = width * height
+                largest = maxOf(largest, surface)
+            }
+        }
+        println(largest)
+    }
+
+    override fun solve2(lines: List<String>) {
+        val points = parsePoints(lines)
+        val borders = parseBorders(points)
+
+        var largest = 0L
+        for (p1 in points) {
+            for (p2 in points) {
+                if (p1 != p2) {
+                    // Make the area 1 smaller, so the borders are removed (this is where borders can be)
+                    val minX = minOf(p1.x, p2.x) + 1
+                    val maxX = maxOf(p1.x, p2.x) - 1
+                    val minY = minOf(p1.y, p2.y) + 1
+                    val maxY = maxOf(p1.y, p2.y) - 1
+                    // What if we take all areas between two points and check if there is any borderline hitting a point inside the area that is not its border?
+                    if (hasNoBordersInside(Block(minX..maxX, minY..maxY), borders)) {
+                        val width = (p1.x - p2.x).absoluteValue + 1L
+                        val height = (p1.y - p2.y).absoluteValue + 1L
+                        val surface = width * height
+                        largest = maxOf(largest, surface)
+                    }
+                }
+            }
+        }
+        println(largest)
+    }
+
+    private fun hasNoBordersInside(
+        block: Block,
+        borders: Set<Point>,
+    ): Boolean {
+        for (border in borders) {
+            if (block.contains(border)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun parsePoints(lines: List<String>): List<Point> {
+        return lines.map { line ->
+            val (x, y) = line.split(",").map { it.toInt() }
+            Point(x, y)
+        }
+    }
+
+    private fun parseBorders(points: List<Point>): Set<Point> {
+        val borders = mutableSetOf<Point>()// Block?
+        for ((from, to) in points.zipWithNext() + listOf(points.last() to points.first())) {
+            val minX = minOf(from.x, to.x)
+            val maxX = maxOf(from.x, to.x)
+            val minY = minOf(from.y, to.y)
+            val maxY = maxOf(from.y, to.y)
+            for (x in minX..maxX) {
+                for (y in minY..maxY) {
+//                    println("x: $x, y: $y")
+                    borders.add(Point(x, y))
+                }
+            }
+        }
+        return borders
+    }
+}
